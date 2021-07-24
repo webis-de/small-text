b2bb210021dbb93166a6b2ff0a5bd83c8a46aa87
Christopher Schröder <schroeder@informatik.uni-leipzig.de>
2021-07-24 15:48:17 +0200

    Add fallback for k-means++ initialization
    Signed-off-by: Christopher Schröder <schroeder@informatik.uni-leipzig.de>

---
+++ small_text/utils/clustering.py
@@ -0,0 +1,67 @@
+import warnings
+import numpy as np
+
+from numpy.random import choice
+from sklearn.cluster import kmeans_plusplus
+
+
+def init_kmeans_plusplus_safe(x, n_clusters, *, x_squared_norms=None, random_state=None,
+                              n_local_trials=None, suppress_warning=False):
+    """
+    Calls scikit-learn's k-means++ initialization with a fallback that prevents duplicate
+    cluster centers. If duplicate indices are encountered, they are replaced with randomly sampled
+    indices and a warning is raised.
+
+    Parameters
+    ----------
+    x : numpy.ndarray
+        The input data to be clustered.
+    n_clusters : int
+        The number of cluster centers to find.
+    x_squared_norms : numpy.ndarray
+        List of squared norms or None.
+    random_state : int or RandomState instance
+        A random state or None.
+    n_local_trials : int
+        Number of local trials.
+    suppress_warning : bool
+        Suppresses the warning given on duplicate indices if True.
+
+    Returns
+    -------
+    centers : numpy.ndarray
+        `n_clusters`-many cluster centers.
+    indices :
+        Indices (relativ to `x`) of the returned cluster centers.
+
+    See Also
+    --------
+    sklearn.cluster import kmeans_plusplus :
+        The actual k-means++ initialization provided by scikit-learn.
+    """
+
+    centers, indices = kmeans_plusplus(x,
+                                       n_clusters,
+                                       x_squared_norms=x_squared_norms,
+                                       random_state=random_state,
+                                       n_local_trials=n_local_trials)
+
+    unique_indices, counts = np.unique(indices, return_counts=True)
+
+    if unique_indices.shape[0] != n_clusters:
+        if not suppress_warning:
+            warnings.warn(f'kmeans_plusplus returned identical cluster centers.')
+
+        remaining_indices = np.arange(x.shape[0])
+        remaining_indices = np.delete(remaining_indices, unique_indices)
+
+        centers = np.delete(centers, np.arange(unique_indices.shape[0])[counts > 1], axis=0)
+
+        fill_indices = choice(remaining_indices, size=n_clusters-unique_indices.shape[0],
+                              replace=False)
+        indices = np.hstack((unique_indices, fill_indices))
+        centers = np.vstack((centers, x[fill_indices]))
+
+        return centers, indices
+
+    return centers, indices
---
+++ tests/unit/small_text/utils/test_clustering.py
@@ -0,0 +1,59 @@
+import unittest
+import numpy as np
+
+from unittest.mock import patch
+
+from small_text.utils.clustering import init_kmeans_plusplus_safe
+
+
+class ClusteringTest(unittest.TestCase):
+
+    @patch('small_text.utils.clustering.warnings.warn')
+    @patch('small_text.utils.clustering.choice')
+    @patch('small_text.utils.clustering.kmeans_plusplus')
+    def test_init_kmeans_plusplus_safe_normal(self, kmeans_plusplus_mock, choice_mock, warn_mock):
+        X = np.random.rand(100, 10)
+
+        result_indices = np.array([0, 1, 2, 3, 4, 5, 6, 7, 42, 99])
+        kmeans_plusplus_mock.return_value = [X[result_indices], result_indices]
+
+        centers, indices = init_kmeans_plusplus_safe(X, 10)
+        kmeans_plusplus_mock.assert_called()
+        choice_mock.assert_not_called()
+        warn_mock.assert_not_called()
+        self.assertEqual((10, 10), centers.shape)
+        self.assertEqual(10, indices.shape[0])
+
+    @patch('small_text.utils.clustering.warnings.warn')
+    @patch('small_text.utils.clustering.choice', wraps=np.random.choice)
+    @patch('small_text.utils.clustering.kmeans_plusplus')
+    def test_init_kmeans_plusplus_safe_duplicate_indices(self, kmeans_plusplus_mock, choice_mock,
+                                                    warn_mock):
+        X = np.random.rand(100, 10)
+
+        result_indices = np.array([0, 1, 2, 3, 4, 5, 6, 7, 42, 42])  # 42 is not unique here
+        kmeans_plusplus_mock.return_value = [X[result_indices], result_indices]
+
+        centers, indices = init_kmeans_plusplus_safe(X, 10)
+        kmeans_plusplus_mock.assert_called()
+        choice_mock.assert_called()
+        warn_mock.assert_called()
+        self.assertEqual((10, 10), centers.shape)
+        self.assertEqual(10, indices.shape[0])
+
+    @patch('small_text.utils.clustering.warnings.warn')
+    @patch('small_text.utils.clustering.choice', wraps=np.random.choice)
+    @patch('small_text.utils.clustering.kmeans_plusplus')
+    def test_init_kmeans_plusplus_safe_duplicate_indices_warning_suppressed(
+            self, kmeans_plusplus_mock, choice_mock, warn_mock):
+        X = np.random.rand(100, 10)
+
+        result_indices = np.array([0, 1, 2, 3, 4, 5, 6, 7, 42, 42])  # 42 is not unique here
+        kmeans_plusplus_mock.return_value = [X[result_indices], result_indices]
+
+        centers, indices = init_kmeans_plusplus_safe(X, 10, suppress_warning=True)
+        kmeans_plusplus_mock.assert_called()
+        choice_mock.assert_called()
+        warn_mock.assert_not_called()
+        self.assertEqual((10, 10), centers.shape)
+        self.assertEqual(10, indices.shape[0])
