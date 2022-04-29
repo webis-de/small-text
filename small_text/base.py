884cf54c5c1d46b2b4000eac82eb6f9b4885b4ea
Christopher Schröder <chschroeder@users.noreply.github.com>
2022-04-29 14:01:24 +0200

    Add docstrings for label constants
    Signed-off-by: Christopher Schröder <chschroeder@users.noreply.github.com>

--- small_text/base.py
+++ small_text/base.py
@@ -2,5 +2,9 @@ import numpy as np
 
 
 LABEL_IGNORED = -np.inf
+"""Value that is used for ignored label.
+"""
 
 LABEL_UNLABELED = -1
+"""Value that is used to indicate an unlabeled example (in the single-class scenario).
+"""
