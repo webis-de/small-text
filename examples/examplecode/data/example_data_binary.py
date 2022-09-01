65c707dd70ea848ddc46d19197f08a8c766aa359
Christopher Schröder <chschroeder@users.noreply.github.com>
2022-09-01 14:50:08 +0200

    Remove redundant normalize in binary classification example
    Signed-off-by: Christopher Schröder <chschroeder@users.noreply.github.com>

--- examples/examplecode/data/example_data_binary.py
+++ examples/examplecode/data/example_data_binary.py
@@ -1,6 +1,4 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import normalize
-
 from small_text.data import SklearnDataset
 
 from examplecode.data.corpus_twenty_news import get_twenty_newsgroups_corpus
@@ -13,7 +11,7 @@ def get_train_test():
 def preprocess_data(train, test):
     vectorizer = TfidfVectorizer(stop_words='english')
 
-    x_train = normalize(vectorizer.fit_transform(train.data))
-    x_test = normalize(vectorizer.transform(test.data))
+    x_train = vectorizer.fit_transform(train.data)
+    x_test = vectorizer.transform(test.data)
 
     return SklearnDataset(x_train, train.target), SklearnDataset(x_test, test.target)
