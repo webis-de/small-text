394936c3974acf59738e7a0ca4d278642542e3c0
Christopher Schröder <chschroeder@users.noreply.github.com>
2023-06-30 23:51:08 +0200

    Add approval tests
    The first approved results were obtained using torch==1.10.0 and torchtext==0.11.0.
    
    Signed-off-by: Christopher Schröder <chschroeder@users.noreply.github.com>

---
+++ tests/approval/test_pytorch_multiclass_classification.py
@@ -0,0 +1,84 @@
+import unittest
+import pytest
+
+import numpy as np
+
+from approvaltests import verify
+from sklearn.metrics import f1_score
+
+from examplecode.data.example_data_multiclass import (
+    get_train_test,
+    preprocess_data
+)
+from examplecode.pytorch_multiclass_classification import load_gensim_embedding, initialize_active_learner
+
+from small_text import KimCNNFactory, BreakingTies, PoolBasedActiveLearner
+from tests.utils.misc import random_seed
+
+
+try:
+    import gensim.downloader as api
+except ImportError:
+    raise RuntimeError('This example requires the gensim library. '
+                       'Please install gensim 3.8.x to run this example.')
+
+
+def evaluate(active_learner, train, test):
+    y_pred = active_learner.classifier.predict(train)
+    y_pred_test = active_learner.classifier.predict(test)
+
+    output = f'Train: f1={f1_score(y_pred, train.y, average="micro"):.4f}\n'
+    output += f'Test: f1={f1_score(y_pred_test, test.y, average="micro"):.4f}\n'
+
+    return output
+
+
+def perform_active_learning(active_learner, train, indices_labeled, test, num_iterations):
+
+    output = ''
+
+    for i in range(num_iterations):
+        output += f'\nIteration {i+1}\n'
+        indices_queried = active_learner.query(num_samples=10)
+
+        y = train.y[indices_queried]
+        active_learner.update(y)
+
+        indices_labeled = np.concatenate([indices_queried, indices_labeled])
+
+        output += evaluate(active_learner, train[indices_labeled], test)
+
+    return output
+
+
+@pytest.mark.pytorch
+class PytorchMulticlassClassificationApprovalTest(unittest.TestCase):
+
+    @random_seed(seed=42, set_torch_seed=True)
+    def test_kimcnn(self, device='cuda'):
+        num_iterations = 3
+
+        # Prepare some data
+        train, test = get_train_test()
+        train, test = preprocess_data(train, test)
+        num_classes = len(np.unique(train.y))
+
+        # Active learning parameters
+        classifier_kwargs = {
+            'embedding_matrix': load_gensim_embedding(train.vocab),
+            'max_seq_len': 512,
+            'num_epochs': 4,
+            'device': device
+        }
+
+        clf_factory = KimCNNFactory('kimcnn', num_classes, classifier_kwargs)
+        query_strategy = BreakingTies()
+
+        # Active learner
+        active_learner = PoolBasedActiveLearner(clf_factory, query_strategy, train)
+        indices_labeled = initialize_active_learner(active_learner, train.y)
+
+        output = \
+        f'{perform_active_learning(active_learner, train, indices_labeled, test, num_iterations)}'
+
+        verify(output)
--- tests/utils/misc.py
+++ tests/utils/misc.py
@@ -1,3 +1,8 @@
+import os
+
+import numpy as np
+
+
 def increase_dense_labels_safe(ds):
     """Increase the labels without leaving the range of the target_labels property.
     The only purpose of this operation is to alter the labels so we can check for a change later."""
@@ -8,3 +13,29 @@ def increase_dense_labels_safe(ds):
     else:
         ds.y = (ds.y + 1) % (ds.y.max() + 1)
     return ds
+
+
+def random_seed(func_or_class=None, seed=42, set_torch_seed=False):
+    import inspect
+    from functools import wraps
+
+    assert 0 <= seed <= 2**32-1
+
+    if func_or_class is not None:
+        if not inspect.isclass(func_or_class) and not inspect.isfunction(func_or_class):
+            raise ValueError('The @random_seed decorator requires a function or class')
+
+    def _decorator(func_or_class):
+        @wraps(func_or_class)
+        def wrapper(*args, **kwargs):
+            os.environ['PYTHONHASHSEED'] = str(seed)
+            if set_torch_seed:
+                import torch
+                torch.manual_seed(seed)
+                torch.backends.cudnn.deterministic = True
+                torch.backends.cudnn.benchmark = False
+            np.random.seed(seed)
+
+            return func_or_class(*args, **kwargs)
+        return wrapper
+    return _decorator(func_or_class) if callable(func_or_class) else _decorator
