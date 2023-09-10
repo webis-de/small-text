d2362248668cef8278aad66d60ac4d5f2b1797b0
Christopher Schröder <chschroeder@users.noreply.github.com>
2023-09-10 23:47:58 +0200

    Simplify kimcnn gradient embedding code
    Signed-off-by: Christopher Schröder <chschroeder@users.noreply.github.com>

--- small_text/integrations/pytorch/classifiers/kimcnn.py
+++ small_text/integrations/pytorch/classifiers/kimcnn.py
@@ -144,7 +144,7 @@ class KimCNNEmbeddingMixin(EmbeddingMixin):
         self.criterion.reduction = 'none'
 
         modules = dict({name: module for name, module in self.model.named_modules()})
-        grad_size = module_selector(modules).weight.size(0)
+        grad_size = module_selector(modules).weight.flatten().size(0)
 
         arr = torch.empty(batch_len, grad_size * self.num_classes)
         for c in range(self.num_classes):
