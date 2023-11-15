Fix elementwise mean vector computation in lightweight coreset.
Signed-off-by: Christopher Schröder <chschroeder@users.noreply.github.com>

small_text/query_strategies/coresets.py
@@ -173,6 +173,7 @@ def lightweight_coreset(x, x_mean, n, batch_size=100, distance_metric='cosine',
                          f'Possible values: {_DISTANCE_METRICS}')
 
     _check_coreset_size(x, n)
+    x_mean = np.expand_dims(x_mean, axis=0)
 
     num_batches = int(np.ceil(x.shape[0] / batch_size))
     indices = np.array([], dtype=int)
@@ -180,7 +181,7 @@ def lightweight_coreset(x, x_mean, n, batch_size=100, distance_metric='cosine',
     for x_batch in np.array_split(x, num_batches, axis=0):
 
         dists = dist_func(x_batch, x_mean, normalized=normalized)
-        dists = np.square(dists)
+        dists = np.square(dists.ravel())
 
         sum_dists = dists.sum()
 

tests/unit/small_text/query_strategies/test_coresets.py
@@ -165,7 +165,7 @@ class _LightweightCoresetFunctionTest(object):
 
     def test_query(self, num_samples=20, num_features=100):
         x = np.random.rand(num_samples, num_features)
-        coreset = lightweight_coreset(x, np.mean(x, axis=0), 10, distance_metric=self.distance_metric)
+        coreset = lightweight_coreset(x, np.mean(x, axis=0), num_samples, distance_metric=self.distance_metric)
         self.assertEqual(coreset.shape, (num_samples,))
 
     def test_query_with_invalid_distance_metric(self, num_samples=20, num_features=100):