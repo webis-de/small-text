61027bbc732e31bfffe5f74e9a4363ef4ca51eb5
Christopher Schröder <chschroeder@users.noreply.github.com>
2023-12-25 15:16:52 +0100

    Remove remnant from previously deprecated early stopping arguments
    Signed-off-by: Christopher Schröder <chschroeder@users.noreply.github.com>

--- small_text/utils/annotations.py
+++ small_text/utils/annotations.py
@@ -74,23 +74,6 @@ def experimental(func_or_class=None):
     return _decorator(func_or_class) if callable(func_or_class) else _decorator
 
 
-def early_stopping_deprecation_warning(early_stopping_no_improvement, early_stopping_acc):
-    if early_stopping_no_improvement != 5:
-        warnings.warn(
-            'early_stopping_no_improvement is deprecated since 1.1.0 '
-            'and will be removed in 2.0.0. Please use the "early_stopping" kwarg in fit () '
-            'instead.',
-            DeprecationWarning,
-            stacklevel=2)
-    if early_stopping_acc != -1:
-        warnings.warn(
-            'early_stopping_acc  is deprecated since 1.1.0 '
-            'and will be removed in 2.0.0. Please use the "early_stopping" kwarg in fit () '
-            'instead.',
-            DeprecationWarning,
-            stacklevel=2)
-
-
 def model_selection_deprecation_warning(model_selection):
     if model_selection is not True:
         warnings.warn(
