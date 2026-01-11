7c107e0f2a31fa8f7c6a83ce34d7fa7cb1752dfa
Christopher Schröder <chschroeder@users.noreply.github.com>
2026-01-11 16:06:02 +0100

    Update year
    Signed-off-by: Christopher Schröder <chschroeder@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ from small_text.version import __version__ as version
 # -- Project information -----------------------------------------------------
 
 project = 'small-text'
-copyright = '2020–2025, Christopher Schröder'
+copyright = '2020–2026, Christopher Schröder'
 author = 'Christopher Schröder'
 
 
